ddca946c54cbb8cfb1b6097bd3cf50f191703ea7
babsonr1 <riley.babson@du.edu>
2021-12-09 12:44:32 -0700

    Added summary statistics.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,12 @@ library(dplyr)
 mechaCar <- read.csv(file="MechaCar_mpg.csv")
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechaCar)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechaCar))
+
+
+suspensionCoil <- read.csv(file="Suspension_Coil.csv")
+total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+total_summary
+
+
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary
